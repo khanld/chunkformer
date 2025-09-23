d22b8904c00d6922dd9022e71541b515a36f6581
khanld <khanhld218@gmail.com>
2025-09-23 16:42:04 +0700

    remove unused files

--- tests/test_model.py
+++ tests/test_model.py
@@ -5,7 +5,7 @@ model = ChunkFormerModel.from_pretrained("khanhld/chunkformer-large-vie")
 
 # For single long-form audio transcription
 transcription = model.endless_decode(
-    audio_path="/home/khanhle/workdir/ASR/ASR/chunkformer/data/common_voice_vi_23397238.wav",
+    audio_path="data/common_voice_vi_23397238.wav",
     chunk_size=64,
     left_context_size=128, 
     right_context_size=128,
